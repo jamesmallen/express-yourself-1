0b47ea2c0a83a29900fb8042e1ca185264966f74
James M. Allen <james.m.allen@gmail.com>
2016-06-29 10:30:11 -0400

    In class solutions

--- ExpressYourself/Parser.cs
+++ ExpressYourself/Parser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExpressYourself
@@ -16,25 +17,54 @@ namespace ExpressYourself
         public static string GetTitle(string str)
         {
             // TODO
-            return "";
+            var re = new Regex(@"Title\: (.*),+");
+            var match = re.Match(str);
+            if (!match.Success)
+            {
+                return "Title Not Found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetType(string str)
         {
-            // TODO
-            return "";
+            var re = new Regex(@"Type: (Book|DVD|Magazine),", RegexOptions.IgnoreCase);
+            var match = re.Match(str);
+
+            if (!match.Success)
+            {
+                return "Type Not Found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetLength(string str)
         {
-            // TODO
-            return "";
+            var re = new Regex(@"Length: ([\w ]+)", RegexOptions.IgnoreCase);
+            var match = re.Match(str);
+
+            if (!match.Success)
+            {
+                return "Length Not Found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static bool IsValidLine(string str)
         {
-            // TODO
-            return false;
+            var re = new Regex(@"Type: (Book|DVD|Magazine),Title\: (.*),+Length: ([\w ]+)", RegexOptions.IgnoreCase);
+            var match = re.Match(str);
+
+            return match.Success;
         }
     }
 }
